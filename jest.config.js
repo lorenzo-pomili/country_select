9191dc7a7e3abd431526358e007b25b04acdd6ae
lorenzo.pomili <pomili.lorenzo85@gmail.com>
2019-10-10 08:18:57 +0200

    mock css import in jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   verbose: true,
   testURL: "http://localhost/",
-  testMatch: [ '**/__tests__/**/*.js?(x)', '**/?(*_)(spec|test).bs.js?(x)' ]
+  testMatch: [ '**/__tests__/**/*.js?(x)', '**/?(*_)(spec|test).bs.js?(x)' ],
+  moduleNameMapper: {
+    "\\.(css|less)$": "identity-obj-proxy"
+  }
 }
